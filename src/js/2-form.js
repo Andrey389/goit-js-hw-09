87bbd509f485b1d4cdf934bb065132f52194b5b0
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-03-16 18:39:14 +0200

    add goit-js-hw-09

---
+++ src/js/2-form.js
@@ -0,0 +1,24 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+
+function formSubmit(event) {
+  event.preventDefault();
+  const email = form.elements.email.value;
+  const message = textarea.value;
+  const data = JSON.stringify({ email, message });
+  localStorage.setItem(STORAGE_KEY, data);
+}
+
+form.addEventListener('submit', formSubmit);
+
+const jsn = localStorage.getItem(STORAGE_KEY) ?? '';
+try {
+  const data = JSON.parse(jsn);
+  console.log(data);
+  form.elements.email.value = data.email;
+  textarea.value = data.message;
+} catch {
+  console.log('No saved data!');
+}
